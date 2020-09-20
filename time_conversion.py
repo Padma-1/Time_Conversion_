d6309c4a1810372e3636fb7199782f38295b4145
Padma-1 <65107839+Padma-1@users.noreply.github.com>
2020-09-20 12:05:10 +0530

    Create time_conversion.py

---
+++ time_conversion.py
@@ -0,0 +1,14 @@
+def timeConversion(s):
+    hh=int(s[0:2])
+    mm=int(s[3:5])
+    ss=int(s[6:8])
+    f=s[8: ]
+    
+    if (f =="PM") and (hh != 12):
+        hh=(hh)+12
+    elif(f=="AM") and hh==12:
+        hh=00
+    print("{:02}:{:02}:{:02}".format(hh,mm,ss))
+    
+s = input()#07:05:45PM
+result = timeConversion(s)
